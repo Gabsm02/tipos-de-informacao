551bf3cfd4384dc66b3804528423470fb591a08b
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-04-30 14:52:34 -0300

    Aprendendo sobre os tipos de informação

---
+++ tipos_informacao.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+String = str("ola, mundo")
+Inteiro = int(10)
+Flutuante = float(10.5)
+Complex = complex(1j)
+Lista = list([1, 2, 3, 4, 5])
+Tupla = tuple((1, 2, 3, 4, 5))
+Range = range(5)
+Dicionario = dict(nome="João", idade=25)
+Set = set({1, 2, 3, 4, 5})
+Frozenset = frozenset({1, 2, 3, 4, 5})
+Booleano = bool(5)
+Bytes = bytes(5)
+ByteArray = bytearray(5)
+Memoria = memoryview(bytes(5))
+
+Data = datetime.today().date()
+
